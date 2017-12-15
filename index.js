262e1932eb812d9ce1a375b8f728a2d6c0c4ec67
AdrianLayton <AdrianLayton@gmail.com>
2017-12-15 17:29:41 -0500

    DB refactor 1

--- index.js
+++ index.js
@@ -10,10 +10,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 AWS.config.update({
   region: "us-east-1",
-  endpoint: "arn:aws:dynamodb:us-east-1:704381840790:table/testDb"
+  endpoint: "arn:aws:dynamodb:us-east-1:704381840790:table/testDB"
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
+
+// let assumeRoleResult = AssumeRole(role-arn);
+// let tempCredentials = new SessionAWSCredentials(
+//    assumeRoleResult.AccessKeyId, 
+//    assumeRoleResult.SecretAccessKey, 
+//    assumeRoleResult.SessionToken);
+// s3Request = CreateAmazonS3Client(tempCredentials);
+
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack); // credentials not loaded
+  else console.log(
+  	"Access Key:", AWS.config.credentials.accessKeyId);
+})
 
 var table = "testDb";
 
@@ -39,9 +52,9 @@ app.post('/', (req,res) => {
 	        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
 	    } else {
 	        console.log("Added item:", JSON.stringify(data, null, 2));
-	    }
-	}
-				)
+}
+}
+)
 })
 
 // res.redirect("/");
